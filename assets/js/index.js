cdbeef74057240031003ba1b39d8cf82b9a4ed9a
Joe Canero <caneroj1@tcnj.edu>
2015-06-11 17:23:20 -0400

    adding projects.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,21 @@
 $(function() {
   $('#main-content').fadeIn(1500);
   $('#my-info').delay(500).fadeIn(1500);
+  var options = [
+    { selector: '#my-projects', offset: 0, callback: 'displayProjectTitle()' },
+    { selector: '#project-1', offset: 0, callback: 'displayProject1()' }
+  ];
+  Materialize.scrollFire(options);
 });
+
+function displayProjectTitle() {
+  $('#my-projects').animate({
+    opacity: 1
+  }, 2300);
+}
+
+function displayProject1() {
+  $('#project-1').animate({
+    opacity: 1
+  }, 2300);
+}
