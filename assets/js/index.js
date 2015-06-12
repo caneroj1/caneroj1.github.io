053e8da11ffd2aecdab7809b0e5e9fcff5c659b7
Joe Canero <caneroj1@tcnj.edu>
2015-06-11 21:27:13 -0400

    more projects.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,8 @@ $(function() {
   $('#my-info').delay(500).fadeIn(1500);
   var options = [
     { selector: '#my-projects', offset: 0, callback: 'displayProjectTitle()' },
-    { selector: '#project-1', offset: 0, callback: 'displayProject1()' }
+    { selector: '#project-1', offset: 0, callback: 'displayProject1()' },
+    { selector: '#project-2', offset: 0, callback: 'displayProject2()' }
   ];
   Materialize.scrollFire(options);
 });
@@ -16,6 +17,13 @@ function displayProjectTitle() {
 
 function displayProject1() {
   $('#project-1').animate({
+    opacity: 1
+  }, 2300);
+}
+
+
+function displayProject2() {
+  $('#project-2').animate({
     opacity: 1
   }, 2300);
 }
