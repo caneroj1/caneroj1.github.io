9f2cf672af9c03117daca9e2f9f9908603bc1b76
Joe Canero <caneroj1@tcnj.edu>
2015-06-11 22:05:36 -0400

    added another project.

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,8 @@ $(function() {
   var options = [
     { selector: '#my-projects', offset: 0, callback: 'displayProjectTitle()' },
     { selector: '#project-1', offset: 0, callback: 'displayProject1()' },
-    { selector: '#project-2', offset: 0, callback: 'displayProject2()' }
+    { selector: '#project-2', offset: 0, callback: 'displayProject2()' },
+    { selector: '#project-3', offset: 0, callback: 'displayProject3()' }
   ];
   Materialize.scrollFire(options);
 });
@@ -24,6 +25,12 @@ function displayProject1() {
 
 function displayProject2() {
   $('#project-2').animate({
+    opacity: 1
+  }, 2300);
+}
+
+function displayProject3() {
+  $('#project-3').animate({
     opacity: 1
   }, 2300);
 }
