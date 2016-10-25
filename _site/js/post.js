a904c12ec6f8a4373de404ad1b395c151f7b93df
caneroj1 <caneroj1@tcnj.edu>
2016-10-24 22:08:03 -0400

    New style
    Minimalistic design and styles. Maybe i’ll add blog posts

--- _site/js/post.js
+++
@@ -1,7 +0,0 @@
-var headerString = $('#banner').data('title');
-geoPattern = GeoPattern.generate(headerString);
-$('#banner').css('background-image', geoPattern.toDataUrl());
-
-$(document).ready(function(){
-   $(".dropdown-button").dropdown();
-});
